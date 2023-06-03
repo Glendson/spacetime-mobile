761e7c7a090f9e6200b0b42ab1b2059d1af2886a
Glendson <gztomazetto@gmail.com>
2023-06-03 14:38:05 +0100

    set fonts on project

--- App.tsx
+++ App.tsx
@@ -1,12 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
 export default function App() {
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar/>
-    </View>
-  );
-}
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
 
+  if (!hasLoadedFonts) {
+    return null
+  }
+
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-900">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+    </View>
+  )
+}
